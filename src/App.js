4faca452d9a48fd84bef6d34af4dcd6d8c1b2d4d
Zemiro014 <zemiro014@gmail.com>
2021-09-13 20:41:11 -0300

    Component have been created to display list of movie information

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
 
 const App = () => {
+
+  // Lista de exibição
+  const [movieList, setMovieList] = useState([]);
 
 useEffect(() =>{
   const loadAll = async () => {
     // Pegando a lista total
     let list = await Tmdb.getHomeList();
-    console.log(list)
+    setMovieList(list);
   }
   loadAll();
 }, []);
 
   return (
-    <div>
-      Olá Mundo !
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
     </div>
   );
 }
---
+++ src/components/MovieRow.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './MovieRow.css';
+
+const MovieRow = ({title, items}) =>{
+    return(
+        <div>
+            <h2>
+                {title}
+            </h2>
+        </div>
+    );
+}
+export default MovieRow
