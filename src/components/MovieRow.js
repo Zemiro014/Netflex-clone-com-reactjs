bb0fdec06b64a6cd64d020d149b5db576733a4f2
Zemiro014 <zemiro014@gmail.com>
2021-09-13 21:21:02 -0300

    App and MovieRow styles

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,13 +1,23 @@
 import React from "react";
-import './MovieRow.css';
+import "./MovieRow.css";
 
-const MovieRow = ({title, items}) =>{
-    return(
-        <div>
-            <h2>
-                {title}
-            </h2>
+const MovieRow = ({ title, items }) => {
+  return (
+    <div className="movieRow">
+      <h2>{title}</h2>
+      <div className="movieRow--listarea">
+        <div className="movieRow--list">
+          {items.results.length > 0 &&  items.results.map((item, key) => (
+              <div key={key} className="movieRow--item">
+                <img
+                  src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
+                  alt={item.original_title}
+                />
+              </div>
+            ))}
         </div>
-    );
-}
-export default MovieRow
+      </div>
+    </div>
+  );
+};
+export default MovieRow;
